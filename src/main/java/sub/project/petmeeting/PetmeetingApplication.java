06822d9b8b7c3ce46235004c6378e79eb32a8f6f
inho-park <ttotto0608@naver.com>
2023-02-02 17:12:26 +0900

    first commit

---
+++ src/main/java/sub/project/petmeeting/PetmeetingApplication.java
@@ -0,0 +1,13 @@
+package sub.project.petmeeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetmeetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetmeetingApplication.class, args);
+    }
+
+}
