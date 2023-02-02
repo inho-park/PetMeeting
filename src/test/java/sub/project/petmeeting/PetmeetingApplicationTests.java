d21b9bb99c1d24e8d3488f632bf99c298feaea75
inho-park <ttotto0608@naver.com>
2023-02-02 17:12:26 +0900

    first commit

---
+++ src/test/java/sub/project/petmeeting/PetmeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package sub.project.petmeeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetmeetingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
