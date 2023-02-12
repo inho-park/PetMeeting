e960c349892d446ed6ccba26c6c1d3e646df83d9
inho-park <ttotto0608@naver.com>
2023-02-13 00:38:23 +0900

    First Commit

---
+++ src/test/java/com/daelim/petmeeting/PetMeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daelim.petmeeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetMeetingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
