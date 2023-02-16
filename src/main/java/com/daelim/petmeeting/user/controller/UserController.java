a8bd725173b245a7e01e74283052e5195be2f3fe
inho-park <ttotto0608@naver.com>
2023-02-16 16:42:10 +0900

    chore : modify gitignore

--- src/main/java/com/daelim/petmeeting/user/controller/UserController.java
+++ src/main/java/com/daelim/petmeeting/user/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.daelim.petmeeting.user.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
