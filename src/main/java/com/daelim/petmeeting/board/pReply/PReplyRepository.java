5b416378b077883ccaa6c4e0175dc7f97b3f75b9
inho-park <ttotto0608@naver.com>
2023-02-18 03:05:48 +0900

    feat : create reply Repository

---
+++ src/main/java/com/daelim/petmeeting/board/pReply/PReplyRepository.java
@@ -0,0 +1,6 @@
+package com.daelim.petmeeting.board.pReply;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PReplyRepository extends JpaRepository<PReply, Long> {
+}
