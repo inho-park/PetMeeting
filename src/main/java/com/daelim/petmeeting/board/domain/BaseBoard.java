7d1c20d3a2204ecec87d61b09054ce920d64e366
inho-park <ttotto0608@naver.com>
2023-02-17 17:12:00 +0900

    refactor : modify package

--- src/main/java/com/daelim/petmeeting/board/domain/BaseBoard.java
+++ src/main/java/com/daelim/petmeeting/board/domain/BaseBoard.java
@@ -17,11 +17,6 @@ public abstract class BaseBoard extends BaseTimeEntity {
     @Column(length = 3000)
     private String content;
 
-    // file 경로를 달아주는 String 을 만들고 나니 AWS S3로 file 의 경로를 어떻게 받아올 수 있는지
-    // 찾아볼 필요가 있다고 느낌 ( 경우에 따라 attach 파일 테이블을 따로 생성해야할 수 있음 )
-    @Column(length = 100)
-    private String file;
-
     @Column(columnDefinition = "default 0")
     private int view;
 }
