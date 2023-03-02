46d74b9ab9dba052f69445ff8eb9eda9c17c685f
inho-park <ttotto0608@naver.com>
2023-03-02 17:17:11 +0900

    feat : getList in PboardController

--- src/main/java/com/daelim/petmeeting/board/pBoard/controller/PboardController.java
+++ src/main/java/com/daelim/petmeeting/board/pBoard/controller/PboardController.java
@@ -1,16 +1,24 @@
 package com.daelim.petmeeting.board.pBoard.controller;
 
+import com.daelim.petmeeting.board.dto.PageRequestDTO;
+import com.daelim.petmeeting.board.pBoard.domain.PBoard;
+import com.daelim.petmeeting.board.pBoard.domain.PBoardRepository;
+import com.daelim.petmeeting.board.pBoard.service.PboardService;
 import com.daelim.petmeeting.pet.domain.PetRepository;
+import com.daelim.petmeeting.user.dto.ResponseDTO;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("/pb")
-public class PboardController {
+import java.util.List;
 
-    @Autowired
-    private PetRepository petRepository;
+@Log4j2
+@RestController
+@RequestMapping("/pb/")
+public class PboardController {
+    private PboardService pboardService;
 
 //    @PostMapping
 //    public ResponseEntity<?> create(@RequestBody CreateRequestDTO dto) {
@@ -22,10 +30,18 @@ public class PboardController {
 //        }
 //    }
 
+    @GetMapping({"/list","/"})
+    public void list(PageRequestDTO pageRequestDTO,
+                     Model model,
+                     boolean pcategory) {
+        log.info("list.............................." + pageRequestDTO);
+        model.addAttribute("response", pboardService.getList(pageRequestDTO, pcategory));
+    }
+
     @GetMapping
-    public ResponseEntity<?> retrieve( ) {
-
-
+    public ResponseEntity<?> retrieve(@ModelAttribute("requestDTO") PageRequestDTO pageRequestDTO,
+                                      boolean category,
+                                      Model model) {
         return null;
     }
 
