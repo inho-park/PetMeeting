66bf2981a27d8f5131b9d2e2619ef7d0c71368ca
JunSeong_Park <ttotto0608@naver.com>
2023-03-03 15:51:20 +0900

    setting react resource

---
+++ src/main/frontend/src/assets/data/userData.jsx
@@ -0,0 +1,36 @@
+const userData = [
+  {
+    userInfo: {
+      id: 1,
+      name: '서동근',
+      email: 'test1@test.com',
+      password: '1q2w3e4r',
+    },
+  },
+  {
+    userInfo: {
+      id: 2,
+      name: '박준성',
+      email: 'test2@test.com',
+      password: '1q2w3e4r',
+    },
+  },
+  {
+    userInfo: {
+      id: 3,
+      name: '김수민',
+      email: 'test3@test.com',
+      password: '1q2w3e4r',
+    },
+  },
+  {
+    userInfo: {
+      id: 4,
+      name: '오유정',
+      email: 'test4@test.com',
+      password: '1q2w3e4r',
+    },
+  },
+];
+
+export default userData;
